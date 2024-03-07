6854f996de9c374cad483076ac8098d9acc45f95
Федор Филиппов <fefilippov@edu.hse.ru>
2024-03-07 22:00:08 +0300

    added extra logging

--- src/main/java/ru/hse/eventProcessing/JFREventProcessor.java
+++ src/main/java/ru/hse/eventProcessing/JFREventProcessor.java
@@ -6,14 +6,11 @@ import jdk.jfr.consumer.RecordedEvent;
 import jdk.jfr.consumer.RecordingFile;
 import ru.hse.XESprocessor.XESSerializerWrapper;
 import ru.hse.config.EventDictionaryReader;
-import ru.hse.config.entity.EventDescription;
 
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class JFREventProcessor {
     public void processEventsFromFile(String filePath, String outputXesFilePath) throws IOException {
@@ -27,17 +24,10 @@ public class JFREventProcessor {
             while (recordingFile.hasMoreEvents()) {
                 RecordedEvent event = recordingFile.readEvent();
 
-//                System.out.println("Event: " + event.getEventType().getName());
-//
-//                List<ValueDescriptor> fields = event.getFields();
-//
-//                for (ValueDescriptor field : fields) {
-//                    Object value = event.getValue(field.getName());
-//
-//                    System.out.println("  " + field.getName() + " (" + field.getTypeName() + "): " + value);
-//                }
-
                 if (eventDescriptions.containsKey(event.getEventType().getName())) {
+
+                    logAllCollectedEventsFromJFRFile(event);
+
                     XEvent convertedEvent = converter.getConvertedEventFromJFRFile(event);
                     serializer.addEventToTrace(convertedEvent);
                 }
@@ -46,6 +36,18 @@ public class JFREventProcessor {
             serializer.serializeLog(outputXesFilePath);
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private void logAllCollectedEventsFromJFRFile(RecordedEvent event) {
+        System.out.println("Event: " + event.getEventType().getName());
+
+        List<ValueDescriptor> fields = event.getFields();
+
+        for (ValueDescriptor field : fields) {
+            Object value = event.getValue(field.getName());
+
+            System.out.println("  " + field.getName() + " (" + field.getTypeName() + "): " + value);
         }
     }
 }
