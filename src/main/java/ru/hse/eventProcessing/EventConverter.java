e9260a48b6e21c82fea2ce776c1007e9d217d48e
Федор Филиппов <fefilippov@edu.hse.ru>
2024-03-07 22:58:39 +0300

    added processing of primitive fields of events

--- src/main/java/ru/hse/eventProcessing/EventConverter.java
+++ src/main/java/ru/hse/eventProcessing/EventConverter.java
@@ -7,9 +7,11 @@ import ext.org.deckfour.xes.model.XAttributeMap;
 import ext.org.deckfour.xes.model.XAttributeTimestamp;
 import ext.org.deckfour.xes.model.XEvent;
 import ext.org.deckfour.xes.model.impl.XAttributeMapImpl;
+import jdk.jfr.ValueDescriptor;
 import jdk.jfr.consumer.RecordedEvent;
 
 import java.util.Date;
+import java.util.List;
 
 public class EventConverter {
 
@@ -32,19 +34,33 @@ public class EventConverter {
         XAttribute attributeEventType = factory.createAttributeLiteral("concept:name", eventType, null);
         attributes.put("concept:name", attributeEventType);
 
-        long threadId;
-        XAttribute attributeThreadId;
-        if (jfrEvent.getThread() != null) {
-            threadId = jfrEvent.getThread().getId();
-            attributeThreadId = factory.createAttributeLiteral("Thread ID", String.valueOf(threadId), null);
-        } else {
-            attributeThreadId = factory.createAttributeLiteral("Thread ID", "null", null);
-        }
-
-        attributes.put("Thread ID", attributeThreadId);
+        processEventFields(jfrEvent, attributes);
 
         event.setAttributes(attributes);
 
         return event;
+    }
+
+    private void processEventFields(RecordedEvent event, XAttributeMap attributes) {
+        List<ValueDescriptor> fields = event.getFields();
+
+        for (ValueDescriptor field : fields) {
+            if (field.getName().equals("startTime")) {
+                continue;
+            }
+            Object value = event.getValue(field.getName());
+
+            if (checkIfTypeNamePrimitive(field.getTypeName())) {
+                XAttribute attributeEventType = factory.createAttributeLiteral(field.getName(), value.toString(), null);
+                attributes.put(field.getName(), attributeEventType);
+            }
+
+            // TODO: add processing of complex types
+        }
+    }
+
+    private boolean checkIfTypeNamePrimitive(String name) {
+        return name.equals("long") || name.equals("int") || name.equals("float")
+                || name.equals("boolean") || name.equals("double") || name.equals("char");
     }
 }
